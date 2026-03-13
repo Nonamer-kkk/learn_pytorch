6cf5aa971155dd9e69fc7a8767ec55f215cb8269
ryw <24371119@buaa.edu.cn>
2026-03-13 23:22:05 +0800

    Changes

---
+++ train_gpu_2.py
@@ -0,0 +1,86 @@
+import torch
+import torchvision
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from model import *
+# 准备数据集
+train_data = torchvision.datasets.CIFAR10(root="./data_model", train=True, transform=torchvision.transforms.ToTensor(),
+                                          download=True)
+test_data = torchvision.datasets.CIFAR10(root="./data_model", train=False, transform=torchvision.transforms.ToTensor(),
+                                         download=True)
+
+device = torch.device("cuda:0")
+# 数据集长度
+train_data_size = len(train_data)
+test_data_size = len(test_data)
+print("训练数据集长度为：{}".format(train_data_size))
+print("测试数据集长度为：{}".format(test_data_size))
+
+# 利用Dataloader加载数据集
+train_dataloader = DataLoader(train_data, batch_size=64)
+test_dataloader = DataLoader(test_data, batch_size=64)
+
+# 搭建神经网络
+test = Test()
+test = test.to(device)
+
+# 损失函数
+loss_fn = nn.CrossEntropyLoss()
+loss_fn = loss_fn.to(device)
+
+# 优化器
+learning_rate = 1e-2
+optimizer = torch.optim.SGD(test.parameters(), lr=learning_rate)
+
+# 设置训练网络的一些参数
+# 记录训练次数
+total_train_step = 0
+# 记录测试次数
+total_test_step = 0
+# 训练的轮数
+epoch = 10
+
+writer = SummaryWriter("./log_test")
+for i in range(epoch):
+    print("------------------第{}轮训练开始------------------".format(i))
+    # 训练步骤开始
+    test.train()
+    for data in train_dataloader:
+        imgs, targets = data
+        imgs = imgs.to(device)
+        targets = targets.to(device)
+        outputs = test(imgs)
+        loss = loss_fn(outputs, targets)
+        # 优化器调优
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        total_train_step = total_train_step + 1
+        if total_train_step % 100 == 0:
+            print("训练次数：{}， loss：{}".format(total_train_step, loss.item()))
+            writer.add_scalar("train_loss", loss.item(), total_train_step)
+
+    test.eval()
+    total_test_loss = 0
+    total_accuracy = 0
+    with torch.no_grad():
+        for data in test_dataloader:
+            imgs, targets = data
+            imgs = imgs.to(device)
+            targets = targets.to(device)
+            outputs = test(imgs)
+            loss = loss_fn(outputs, targets)
+            total_test_loss += loss.item()
+            accuracy = (outputs.argmax(1) == targets).sum()
+            total_accuracy += accuracy
+    print("整体测试集上的Loss：{}".format(total_test_loss))
+    print("整体测试集上的正确率：{}".format(total_accuracy/test_data_size))
+    writer.add_scalar("test_loss", total_test_loss, total_test_step)
+    writer.add_scalar("test_accuracy", total_accuracy/test_data_size, total_test_step)
+    total_test_step += 1
+
+writer.close()
+
+
